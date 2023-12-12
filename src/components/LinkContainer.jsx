917abe61d64ff5acdb90b39df267364ae99a5059
bolaji oke <bolaoke228062@gmail.com>
2023-12-12 14:27:48 -0500

    Update client for db

--- src/components/LinkContainer.jsx
+++ src/components/LinkContainer.jsx
@@ -1,9 +1,33 @@
 import Table from './Table'
 import Form from './Form'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+const getLinks = async () => {
+    try {
+        // make a request = await fetch('/api/links')
+        const response = await fetch('http://localhost:3000/api/links')
+        //convert the response to json
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.error(error)
+    }
+}
 
 function LinkContainer() {
     const [links, setLinks] = useState([])
+
+    useEffect(async () => {
+        // const links = await getLinks()
+        // console.log('LINKS FROM SERVER', links)
+        // const updated = links.map((e) => ({
+        //     id: e.id,
+        //     name: e.name,
+        //     URL: e.url,
+        // }))
+        // console.log(updated)
+        setLinks([])
+    }, [links])
 
     const handleRemove = (index) => {
         const newLinks = links.filter((v, k) => k !== index)
